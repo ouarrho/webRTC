16ab5a38b41ab358a368a79859689bf456ae9adb
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2023-08-01 22:05:21 +0100

    --

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+io.on('connection', (socket) => {
+    socket.on('message', (message, username) =>     {
+        io.emit('message', username, message);   
+    });
+});
+
+http.listen(8080, () => console.log('listening on http://localhost:8080') );
+
+
+// Regular Websockets
+
+// const WebSocket = require('ws')
+// const server = new WebSocket.Server({ port: '8080' })
+
+// server.on('connection', socket => { 
+
+//   socket.on('message', message => {
+
+//     socket.send(`Roger that! ${message}`);
+
+//   });
+
+// });
+
+
+ 
